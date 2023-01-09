1b465d985e32ad113f0f598b39a52eee6a707e49
AlvaradoHarlam-FS <hjalvarado@student.fullsail.edu>
2023-01-09 18:02:27 -0500

    complete

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+import React from "react";
+
+export default function Home() {
+  return (
+    // eslint-disable-next-line react/no-unescaped-entities
+    <h1> Next. js is not very flexible when it comes to routing. The default approach is page-based, where you specify whether to generate those pages server-side, client-side or static. While this is good for simple applications, if you need something more complex, you'll need to write a lot more code and make use of Node </h1>
+  )
+}
