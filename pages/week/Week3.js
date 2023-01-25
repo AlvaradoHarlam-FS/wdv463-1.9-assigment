b3c94cfef2817a0d518622c7a31c2bf31bc0f265
AlvaradoHarlam-FS <hjalvarado@student.fullsail.edu>
2023-01-25 17:39:42 -0500

    Week4

--- pages/week/Week3.js
+++ pages/week/Week3.js
@@ -4,7 +4,8 @@ import api from '../../public/week3.jpg'
 function Week3() {
 
   return     <div>
-  <h1> What Are Deployment Systems? 
+    <h1>What Are Deployment Systems? </h1>
+  <p className="week4"> 
 Deployment Platforms are a category of software developed by third-party vendors that mimic the features and functionalities of Online Staffing firms, often offering the software as a white-labeled “app.” They typically include
 Worker profiles
  Work history 
@@ -14,7 +15,7 @@ Candidate availability tracking and mobile check-in
 Candidate and client ratings
 In essence, they are software vendors that try to bring an “Uber-like” experience to a staffing firm’s clients and candidates.
 
-The majority of Deployment Software focuses on short-term, hourly, or shift-based assignments, common in hospitality staffing, light industrial, retail, healthcare, and education. While the technology was originally built to mimic capabilities of Online Staffing firms that specialized in on-demand labor (e.g., BlueCrew, Wonolo, Jobble, et al.), we categorize these firms as a sub-vertical of Tracking Systems. This category includes Applicant Tracking Systems (ATS), Freelancer Management Systems (FMS), and Vendor Management Systems (VMS).</h1>
+The majority of Deployment Software focuses on short-term, hourly, or shift-based assignments, common in hospitality staffing, light industrial, retail, healthcare, and education. While the technology was originally built to mimic capabilities of Online Staffing firms that specialized in on-demand labor (e.g., BlueCrew, Wonolo, Jobble, et al.), we categorize these firms as a sub-vertical of Tracking Systems. This category includes Applicant Tracking Systems (ATS), Freelancer Management Systems (FMS), and Vendor Management Systems (VMS).</p>
   <Image src={api}/>
  
   </div>
--- pages/week/Week4.js
+++ pages/week/Week4.js
@@ -1,7 +1,23 @@
+import Image from "next/image";
+import api from '../../public/week4.jpg'
+
 function Week4() {
 
-  return <h1>testing</h1>
-  
-}
+  return     <div>
+  <h1 className="week"> Building a product that requires passwords for user authentication means that you need to implement a method to verify your users’ passwords. However, storing plaintext passwords in your database comes with significant security risks. 
 
+Holding this data opens your app up to risk if your database gets breached. Hackers — who can be rogue employees that have access to the database — benefit from these attacks because of the prevalence of users re-using the same password across different websites. Even if your application isn’t particularly security sensitive, attackers hope your users have used those passwords at higher value accounts like their bank accounts. Securely storing passwords is essential to mitigate these risks.
+
+Password hashing is a strategy to ensure that passwords are stored securely. In this blog post, we’ll explain what hashing is, why it’s important, and how Stytch has implemented it to make our Passwords product as modern and secure as possible.
+
+The basics of password hashing
+Password hashing is the practice of algorithmically turning a password into ciphertext, or an irreversibly obfuscated version of itself, as a means of blocking against the threat of password breaches. Essentially, one string of characters in a password is transformed into a completely different string using a mathematical hashing function. Once a string (password) has been hashed, there’s no way to reverse the process and each time a user logs in, their hashed password is compared with a recorded hashed value. 
+
+That might sound a bit technical, so let’s put hashing into human terms — say you have two users, Alice and Bob, who create an account on your website or platform. When they finish registering with a username/email and password, you store their passwords in a database. 
+
+When you’re storing these passwords, the worst thing you could do is store them in plain text. Storing in plain text means you’re directly tying your user’s identity to their password in a readable format, as seen in the image below:</h1>
+  <Image src={api}/>
+ 
+  </div>
+}
 export default Week4
